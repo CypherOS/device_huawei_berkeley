15484287438d671bb1a75f73e43672c53fe02b10
Paul Keith <javelinanddart@gmail.com>
2018-05-09 02:21:19 +0200

    berkeley: Implement a dummy IExtTelephony service
    * To implement manual SIM provisioning
    
    Change-Id: I9bca42c0d7d5d60f3cc96f6a4b75ad569e9a787f

--- device.mk
+++ device.mk
@@ -89,6 +89,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     qti-telephony-common
 
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Recovery
 PRODUCT_PACKAGES += \
     resize2fs_static

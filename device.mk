9a963bfa127359dcb81fe33aa43a70475ee06484
LuK1337 <priv.luk@gmail.com>
2018-05-26 11:55:29 +0200

    berkeley: Set proper boot animation size

--- device.mk
+++ device.mk
@@ -21,5 +21,9 @@ $(call inherit-product-if-exists, vendor/huawei/berkeley/berkeley-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2160
+TARGET_SCREEN_WIDTH := 1080
+
 # Inherit from kirin970-common
 $(call inherit-product, device/huawei/kirin970-common/kirin970.mk)
